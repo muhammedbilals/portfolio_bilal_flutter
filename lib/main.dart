f43d766c215777c8de7a4a21d84da58f19fd3ece
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-03-11 11:02:00 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
@@ -9,7 +10,9 @@ import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/gestures.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MultiProvider(
     providers: [ChangeNotifierProvider(create: (_) => SideMenuController())],
     child: const MyApp(),
---
+++ lib/services/firebase_storage_services.dart
@@ -0,0 +1,14 @@
+getAvatarUrlForProfile(String imageFileName) async {
+final FirebaseStorage firebaseStorage = FirebaseStorage(
+    app: Firestore.instance.app,
+    storageBucket: 'gs://your-firebase-app-url.com');
+
+Uint8List imageBytes;
+await firebaseStorage
+    .ref()
+    .child(imageFileName)
+    .getData(100000000)
+    .then((value) => {imageBytes = value})
+    .catchError((error) => {});
+return imageBytes;
+}
